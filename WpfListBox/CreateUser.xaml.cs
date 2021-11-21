e714bb40049ef7bacf617ed9e75a9c15af3f3d1d
Alexander <vyshky2@gmail.com>
2021-11-21 18:12:42 +0400

    Add project files.

---
+++ WpfListBox/CreateUser.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfListBox
+{
+    public partial class CreateUser : Window
+    {
+        public CreateUser()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Add(object sender, RoutedEventArgs e)
+        {
+
+            if (!Int32.TryParse(TextBoxPassword.Text, out int password)) { return; }
+
+            User newUser = new User
+            {
+                FirstName = TextBoxFirstName.Text,
+                Lastname = TextBoxLastName.Text,
+                Password = password
+            };
+            MainWindow.AddToList(newUser);
+        }
+    }
+}
---
+++ WpfListBox/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfListBox
+{
+    public partial class MainWindow : Window
+    {
+        private static ObservableCollection<User> userList;
+        private static int? selectedIdex;
+        public MainWindow()
+        {
+            InitializeComponent();
+            userList = new ObservableCollection<User>();
+            usersGrid.ItemsSource = userList;
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            CreateUser form2 = new CreateUser();
+            form2.Show();
+        }
+
+        public static void AddToList(User user)
+        {
+            userList.Add(user);
+        }
+
+        private void usersGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedIdex = (sender as DataGrid).SelectedIndex;
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedIdex != null && userList.Count != 0)
+            {
+                userList.RemoveAt(selectedIdex.Value);
+            }
+        }
+    }
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public int Password { get; set; }
+    }
+}
